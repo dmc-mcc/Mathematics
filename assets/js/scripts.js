b1ec4af901b383c29941ec463c974f4909812227
dmc mcc <mccarthydonal@hotmail.com>
2024-01-24 22:03:39 +0000

    feat: added event listeners

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,50 @@
-console.log("connected.");
+/*
+wait fpr the page/DOM to load
+add event listeners
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
