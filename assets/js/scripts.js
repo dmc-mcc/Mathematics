665b58e8085fa8c704d34a187b7894891be94b1d
dmc mcc <mccarthydonal@hotmail.com>
2024-01-24 22:28:51 +0000

    feat: dispaly and compute addition question

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,18 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType)
+                //alert(`You clicked ${gameType}`);
             }
         })
     }
+    runGame("addition")
 })
 
 /**
  * Main Game Loop
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.ceil(Math.random()*25);
     let num2 = Math.ceil(Math.random()*25);
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`unknown game ${gameType}`);
+        throw `unknown game ${gameType}. Abort, Abort`;
+    }
 
 }
 
@@ -42,7 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion( op1, op2) {
+    document.getElementById("operand1").textContent = op1;
+    document.getElementById("operand2").textContent = op2;
+    document.getElementById("operator").textContent = '+';
 
 }
 
